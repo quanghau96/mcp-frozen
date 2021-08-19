fd6ccf230dc95cb3c92f212df7766bb1736ddbc8
Hau Vo <hau.vo@s3corp.com.vn>
2021-08-19 15:05:28 +0700

    setup

---
+++ src/bootstrap.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+const mount = (el) => {
+  ReactDOM.render(
+    <App />,
+    el
+  );
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_frozen-app');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+export { mount };
